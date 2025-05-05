f1033c595d4bc18ed6e309e4cb5986e3604b871f
Abdulkarim <abdyabkor@gmail.com>
2025-05-05 17:11:35 +0300

    add one route

--- src/route/message.route.js
+++ src/route/message.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteMessage, getMessage, getUnsentMessages, sendMessage, updateMessage, updateToRead } from "../controller/message.controller.js";
+import { checkSentMessage, deleteMessage, getMessage, getUnsentMessages, sendMessage, updateMessage, updateToRead } from "../controller/message.controller.js";
 import upload from "../lib/multer.js";
 import protect from "../middleware/auth.middleware.js";
 
@@ -7,9 +7,11 @@ import protect from "../middleware/auth.middleware.js";
 const messageRoute = express.Router();
 
 
-messageRoute.get("/", protect, getMessage);
+messageRoute.post("/get-messages", protect, getMessage);
 
 messageRoute.get("/get-unsent-message", protect, getUnsentMessages);
+
+messageRoute.post("/check-sent-message", protect, checkSentMessage);
 
 messageRoute.post("/update-message", protect, updateToRead);
 
