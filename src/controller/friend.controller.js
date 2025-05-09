de0f5be76eec642b2e7dda0b7d57e0e41a3a5f67
Abdulkarim <abdyabkor@gmail.com>
2025-05-09 23:35:39 +0300

    add query search function

--- src/controller/friend.controller.js
+++ src/controller/friend.controller.js
@@ -1,4 +1,6 @@
+import { distance } from "fastest-levenshtein";
 import Friend from "../model/friend.model.js";
+import User from "../model/user.model.js";
 
 const getFriends = async (req, res) => {
     try {
@@ -9,6 +11,28 @@ const getFriends = async (req, res) => {
         res.status(500).json({ message: e.message });
     }
 };
+
+const search = async (req, res) => {
+    try {
+        let result = [];
+        const userId = req.id;
+        const query = req.body.query;
+        const users = await User.find({ _id: { $ne: userId } }, { password: 0, email: 0 });
+        result = users
+            .map(ele => {
+                return {
+                    ...ele._doc,
+                    dis: distance(ele.userName, query)
+                }
+            })
+            .sort((a, b) => a.dis - b.dis)
+            .slice(0, 20);
+        res.status(201).json(result);
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+}
 
 const addFriend = async (req, res) => {
     try {
@@ -35,4 +59,4 @@ const deleteFriend = async (req, res) => {
 
 
 
-export { getFriends, addFriend, deleteFriend };
+export { getFriends, search, addFriend, deleteFriend };
