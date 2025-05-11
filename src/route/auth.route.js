516765e81f5f3deef88207d1a3478327a8fcd53a
Abdulkarim <abdyabkor@gmail.com>
2025-05-11 22:17:09 +0300

    add routes to auth

--- src/route/auth.route.js
+++ src/route/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { checkAuth, login, logout, signup } from "../controller/auth.controller.js";
+import { changePassword, changePhoto, changeUsername, checkAuth, login, logout, signup } from "../controller/auth.controller.js";
+import protect from "../middleware/auth.middleware.js";
+import upload from "../lib/multer.js";
 
 
 const authRoute = express.Router();
@@ -9,7 +11,13 @@ authRoute.post("/signup", signup)
 
 authRoute.post("/login", login)
 
-authRoute.post("/logout", logout)
+authRoute.post("/logout", protect, logout)
+
+authRoute.post("/change-photo", protect, upload.single('avatar'), changePhoto)
+
+authRoute.post("/change-username", protect, changeUsername)
+
+authRoute.post("/change-password", protect, changePassword)
 
 authRoute.get("/auth-state", checkAuth)
 
